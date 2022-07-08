6b31d4448c63f2fd919f55f5d0fb6328665fe4df
gabija savu <gabija.savu@gmail.com>
2022-07-08 22:21:46 +0300

    on code change reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,42 @@
-const path = require("path");
+const path = require('path');
 
+// https://medium.com/@raviroshan.talk/webpack-understanding-the-publicpath-mystery-aeb96d9effb1
 module.exports = {
-  mode: "none",
-  devtool: "eval-source-map",
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        include: [path.resolve(__dirname, "src")],
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          { loader: "style-loader" },
-          {
-            loader: "css-loader",
-            options: { sourceMap: true, importLoaders: 1 },
-          },
-          { loader: "sass-loader", options: { sourceMap: true } },
+    mode: 'none',
+    devtool: 'eval-source-map',
+    entry: './src/index.ts',
+    devServer: {
+        historyApiFallback: true,
+        open: true,
+        compress: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                include: [path.resolve(__dirname, 'src')],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    { loader: 'style-loader' },
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true, importLoaders: 1 },
+                    },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ],
+            },
         ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    publicPath: "public",
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        publicPath: '/',
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
 };
